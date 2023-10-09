c08ddd9d9f1f89e2954e7bda872c8610665a719a
Konrad <konx90@gmail.com>
2023-10-09 18:58:30 +0200

    Init

---
+++ src/main/java/com/Clone/LubimyCzytacClone/LubimyCzytacCloneApplication.java
@@ -0,0 +1,13 @@
+package com.Clone.LubimyCzytacClone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LubimyCzytacCloneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LubimyCzytacCloneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Clone/LubimyCzytacClone/LubimyCzytacCloneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Clone.LubimyCzytacClone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LubimyCzytacCloneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
